c0faefb0f0e1f99eb61e4cd9c11beac7d6797bc4
Barri <info@bdesignstudio.co.uk>
2020-04-26 17:54:17 +0100

    status

--- game.js
+++ game.js
@@ -16,15 +16,18 @@ while (isSunk == false) {
     guesses += 1;
 
     if (guess == location1 || guess == location2 || guess == location3) {
+      alert("HIT!");
       hits += 1;
       if (hits == 3) {
         isSunk = true;
-        alert("You sank my battleship!!");
+        alert("You sank my battleship!!");}
+      } else {
+        alert('MISS');
       }
     }
   }
-};
-var stats = "You took" + guesses + " guesses to sink the battleship, " + " which means your shooting accuracy was " + (3/guesses);
+
+var stats = "You took " + guesses + " guesses to sink the battleship, " + " which means your shooting accuracy was " + (3/guesses);
 alert(stats);
 
 
